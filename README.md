2cef05f8630d27ead20ddac24d738762dab554aa
Tomato4cc <tomato4cc@gmail.com>
2017-09-20 03:15:15 +0300

    Populate repository from offline source

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Crypter
-PES File Crypter re-imagined
+Alternative PES file crypter v1.0. Supports decryption/encryption of PES
+EDIT files and zlibbing/unzlibbing of several filetypes either one-by-one
+or in larger batches inside a folder.
+
+Usage:
+ - Drag-and-Drop the file/folder you want to process and the tool will do
+   the rest.
+ - Decrypter takes an encrypted PES18 EDIT file. Otherwise the external file
+   decrypter18.exe will crash and the tool won't proceed. Still, don't feed
+   it invalid data since something might go wrong.
+ - Encrypter takes a decrypted PES18 EDIT file, usually one created by using
+   the tool's Decrypter function. The tool checks if the header of the file
+   is correct and then assumes the rest is fine and tries to encrypt the
+   file to produce a valid PES18 EDIT file. Feeding the function invalid
+   data will produce an invalid EDIT file.
+ - UnZlib takes either a file or a folder. If the input is a folder the 
+   function will unzlib every supported file inside that folder and place
+   them in the "unzlibbed" folder at the tool's root directory.
+ - Zlib takes either a file or a folder. If the input is a folder the 
+   function will zlib every supported file inside that folder and place
+   them in the "zlibbed" folder at the tool's root directory.
+   
+Building:
+  - .exe releases are built with pyinstaller. The standard build command for
+    a release is `pyinstaller -F -w -i icon.ico crypter.py`. Only the .py, 
+	the .ico and the **ui** folder are required for building.
---
+++ crypter.py
@@ -0,0 +1,241 @@
+#Some kind of an all-in-one tool for a handful of PES file formats.
+#So far can encrypt/decrypt EDIT files and zlib/unzlib texture .dds files
+#either one at a time or in bulk.
+#More features to be added probably soon maybe.
+
+import os
+import shutil
+import sys
+import subprocess
+import zlib
+import struct
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from ui.ui import Ui_Crypter
+
+class Crypter(QMainWindow, Ui_Crypter):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setWindowIcon(QIcon('img/icon.png'))
+
+        #Connect signals
+        self.decryb.dropped.connect(self.decrypt)
+        self.encryb.dropped.connect(self.encrypt)
+        self.unzlibb.dropped.connect(self.unzlib)
+        self.zlibb.dropped.connect(self.zlib)
+        
+        self.root = os.path.dirname(os.path.realpath(__file__)) #Store the script root dir
+        self.temp = os.path.join(self.root, 'temp') #Store the temp dir
+        self.unzl = os.path.join(self.root, 'unzlibbed') #Store the dir for batch unzlibbed files
+        self.zl = os.path.join(self.root, 'zlibbed') #Store the dir for batch zlibbed files
+    
+    #Decrypt an editfile
+    def decrypt(self, urll):
+        for url in urll:
+            if(os.path.isfile(url)):
+                os.makedirs(self.temp, exist_ok=True) #Make temp directory since it's needed
+                #Attempt to decrypt the file
+                subprocess.call([os.path.join(self.root, 'lib', 'decrypter18.exe'), url, self.temp])
+                #Very ugly way of checking if all components exist but it'll do for now
+                if(os.path.isfile(os.path.join(self.temp, 'description.dat')) and os.path.isfile(os.path.join(self.temp, 'header.dat')) and os.path.isfile(os.path.join(self.temp, 'encryptHeader.dat')) and os.path.isfile(os.path.join(self.temp, 'version.txt')) and os.path.isfile(os.path.join(self.temp, 'logo.png')) and os.path.isfile(os.path.join(self.temp, 'data.dat'))):
+                    fname = os.path.basename(url) #Get filename from url
+                    #Open destination file for writing
+                    o = open(os.path.join(self.root, "decrypted_" + fname), 'wb')
+                    #Open description.dat for reading
+                    rdesc = open(os.path.join(self.temp, "description.dat"), 'rb')
+                    o.write(rdesc.read()) #Write desc into decrypted file
+                    rdesc.close()
+                    
+                    #Open header.dat for reading
+                    rhead = open(os.path.join(self.temp, "header.dat"), 'rb')
+                    o.write(rhead.read()) #Write head into decrypted file
+                    rhead.close()
+                    
+                    #Open encryptionHeader.dat for reading
+                    enchead = open(os.path.join(self.temp, "encryptHeader.dat"), 'rb')
+                    o.write(enchead.read()) #Write encryption header into decrypted file
+                    enchead.close()
+                    
+                    #Open version.txt for reading
+                    vtext = open(os.path.join(self.temp, "version.txt"), 'rb')
+                    o.write(vtext.read()) #Write version string into decrypted file
+                    vtext.close()
+                    
+                    #Open logo.png for reading
+                    logo = open(os.path.join(self.temp, "logo.png"), 'rb')
+                    o.write(logo.read()) #Write logo into decrypted file
+                    logo.close()
+                    
+                    #Open data.dat for reading
+                    data = open(os.path.join(self.temp, "data.dat"), 'rb')
+                    o.write(data.read()) #Write data into decrypted file
+                    data.close()
+                    
+                    #All done here, run cleanup
+                    o.close()
+                    #Remove temp directory
+                    shutil.rmtree(self.temp, ignore_errors=True)
+
+    #Encrypt an editfile
+    def encrypt(self, urll):
+        for url in urll:
+            if(os.path.isfile(url)):
+                b = open(url, 'rb') #Open file for reading
+                head = b.read(9).decode('utf-8') #Read "header" from file
+                #Check if file at least starts correctly.
+                if(head == "Edit Data"):
+                    _DESCL = 384 #Description length
+                    _HEADL = 208 #Header length
+                    _ENCHL = 320 #Encryption header length
+                    _VERSL = 90 #Version string length
+                    _LOGOL = 66179 #Logo length, static for now
+                    #Then figure data length from the other lengths
+                    _DATAL = os.stat(url).st_size - _DESCL - _HEADL - _ENCHL - _VERSL - _LOGOL
+                    
+                    fname = os.path.basename(url) #Get filename from url
+                    
+                    '''#Figure out logo length. No idea why anyone would want to
+                    #change this but do it for consistency's sake
+                    b.seek(_DESCL + _HEADL + _ENCHL + _VERL) #Skip other data
+                    b.seek(8, 1) #Skip PNG header
+                    pnglen = 0
+                    while(true):
+                        clen = '''
+                        
+                    os.makedirs(self.temp, exist_ok=True) #Make temp directory since it's needed
+                    b.seek(0) #Get back to the beginning of the source file
+                    
+                    #Open description.dat for writing
+                    o = open(os.path.join(self.temp, 'description.dat'), 'wb')
+                    o.write(b.read(_DESCL)) #Write description into file
+                    o.close()
+                    
+                    #Open header.dat for writing
+                    o = open(os.path.join(self.temp, 'header.dat'), 'wb')
+                    o.write(b.read(_HEADL)) #Write header into file
+                    o.close()
+                    
+                    #Open encryptHeader.dat for writing
+                    o = open(os.path.join(self.temp, 'encryptHeader.dat'), 'wb')
+                    o.write(b.read(_ENCHL)) #Write encrypt header into file
+                    o.close()
+                    
+                    #Open version.txt for writing
+                    o = open(os.path.join(self.temp, 'version.txt'), 'wb')
+                    o.write(b.read(_VERSL)) #Write version string into file
+                    o.close()
+                    
+                    #Open logo.png for writing
+                    o = open(os.path.join(self.temp, 'logo.png'), 'wb')
+                    o.write(b.read(_LOGOL)) #Write logo into file
+                    o.close()
+                    
+                    #Open data.dat for writing
+                    o = open(os.path.join(self.temp, 'data.dat'), 'wb')
+                    o.write(b.read(_DATAL)) #Write data into file
+                    o.close()
+                    
+                    #All done, run cleanup
+                    b.close()
+                    #Attempt to encrypt the file
+                    subprocess.call([os.path.join(self.root, 'lib', 'encrypter18.exe'), self.temp, os.path.join(self.root, "encrypted_" + fname)])
+                    #Remove temp directory
+                    shutil.rmtree(self.temp, ignore_errors=True)
+                    
+    #Unzlib a file
+    def unzlib(self, urll):
+        for url in urll:
+            if(os.path.isfile(url)):
+                o = open(url, 'rb')
+                if(os.stat(url).st_size > 0): #Make sure there's something to read
+                    o.seek(3, 0) #Seek to the header
+                    head = o.read(5) #Read the header
+                    #Check to make sure the file is zlibbed
+                    if(head == b'WESYS'):
+                        o.seek(16,0) #Skip the file header and the PES header
+                        dat = o.read() #Read the actual compressed data
+                        decomp = open(os.path.join(os.path.dirname(url), "unzlibbed_" + os.path.basename(url)), 'wb')
+                        cdat = zlib.decompress(dat, 32) #Decompress the data
+                        decomp.write(cdat)
+                        decomp.flush()
+                        decomp.close()
+                    o.close()
+            #Also support batch unzlibbing if the dragged object is a dir
+            elif(os.path.isdir(url)):
+                exts = ['.dds', '.bin', '.xml', '.mtl'] #List of filetypes to process
+                os.makedirs(self.unzl, exist_ok=True) #Make temp directory since it's needed
+                for r, d, f in os.walk(url): #Go through the whole folder
+                    for file in f:
+                        if(any(e in file.lower() for e in exts)): #Match the file extensions list to current file
+                            o = open(os.path.join(r, file), 'rb')
+                            if(os.stat(os.path.join(r, file)).st_size > 0): #Make sure there's something to read
+                                o.seek(3, 0) #Seek to the header
+                                head = o.read(5) #Read the header
+                                if(head == b'WESYS'):
+                                    o.seek(16,0) #Skip the file header and the PES header
+                                    dat = o.read() #Read the actual compressed data
+                                    decomp = open(os.path.join(self.unzl, file), 'wb')
+                                    cdat = zlib.decompress(dat, 32) #Decompress the data
+                                    decomp.write(cdat) #Write decompressed data
+                                    decomp.flush()
+                                    decomp.close()
+                                o.close()
+    
+    #Zlib a file
+    def zlib(self, urll):
+        for url in urll:
+            if(os.path.isfile(url)):
+                o = open(os.path.join(url), 'rb') #Open file for reading
+                head = o.read(8) #Store the header of the file
+                o.seek(0,0) #Back to the start
+                dat = o.read() #Store the entire file
+                o.close()
+                h = struct.unpack('2I', head) #Unpack the header for checking
+                
+                #Magic check to make sure the file is not already zlibbed.
+                #PES zlibbed files always start with 0x000101 WESYS
+                if(h[0] != 1459683584 and h[1] != 1398362949):
+                    #File is not zlibbed yet, get to work
+                    comp = open(os.path.join(os.path.dirname(url), "zlibbed_" + os.path.basename(url)), 'wb')
+                    cdat = zlib.compress(dat, 9) #Compress the whole file with the highest level of compression
+                    #Write the compressed data in the PES special format.
+                    #Start with the 0x000101 WESYS, then length of the compressed data, then length of the original data
+                    comp.write(struct.pack("4I",0x57010100, 0x53595345, len(cdat), len(dat)))
+                    comp.write(cdat)
+                    comp.flush()
+                    comp.close()
+                #Also support batch zlibbing if the dragged object is a dir
+            elif(os.path.isdir(url)):
+                exts = ['.dds'] #List of filetypes to process
+                os.makedirs(self.zl, exist_ok=True) #Make temp directory since it's needed
+                for r, d, f in os.walk(url): #Go through the whole folder
+                    for file in f:
+                        if(any(e in file for e in exts)): #Check if current file matches the filetype list
+                            o = open(os.path.join(r, file), 'rb')
+                            head = o.read(8) #Store the header of the file
+                            o.seek(0,0) #Back to the start
+                            dat = o.read() #Store the entire file
+                            o.close()
+                            h = struct.unpack('2I', head) #Unpack the header for checking
+                            
+                            #Magic check to make sure the file is not already zlibbed.
+                            #PES zlibbed files always start with 0x000101 WESYS
+                            if(h[0] != 1459683584 and h[1] != 1398362949): 
+                                #File is not zlibbed yet, get to work
+                                comp = open(os.path.join(self.zl, file), 'wb')
+                                cdat = zlib.compress(dat, 9) #Compress the whole file with the highest level of compression
+                                #Write the compressed data in the PES special format.
+                                #Start with the 0x000101 WESYS, then length of the compressed data, then length of the original data
+                                comp.write(struct.pack("4I",0x57010100, 0x53595345, len(cdat), len(dat)))
+                                comp.write(cdat) #Write compressed data
+                                comp.flush()
+                                comp.close()
+    
+if __name__ == "__main__":
+    p = QApplication(sys.argv)
+    w = Crypter()
+    w.show()
+    sys.exit(p.exec_())
